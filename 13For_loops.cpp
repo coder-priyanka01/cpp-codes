f3f169a87aad538d1370984f278d8c286ed60fa5
Priyanka <211002112+coder-priyanka01@users.noreply.github.com>
2025-07-05 23:24:29 +0530

    Add files via upload

---
+++ 13For_loops.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    /*loops on c++:
+    There are three types of loops in c++:
+    1. For loop
+    2. while loop
+    3. do-while loop
+
+    */
+   
+    /* for loop in c++*/
+    //Syntax for for loop
+    //for (initialization; condition; updation)
+    // {
+    //  loop body(C++ code);
+    // }
+
+    for (int i = 1; i <= 40; i++)
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+}
